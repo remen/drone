955197bbc03c0897f497ec8b9b49cd3d53586870
Petter Remen <petter.remen@meltwater.com>
2021-10-07 17:00:44 +0200

    fix: Don't set environment on github deployment status update

--- service/status/status.go
+++ service/status/status.go
@@ -76,7 +76,6 @@ func (s *service) Send(ctx context.Context, user *core.User, req *core.StatusInp
 			Desc:        createDesc(req.Build.Status),
 			State:       convertStatus(req.Build.Status),
 			Target:      fmt.Sprintf("%s/%s/%d", s.base, req.Repo.Slug, req.Build.Number),
-			Environment: req.Build.Target,
 		})
 		return err
 	}
